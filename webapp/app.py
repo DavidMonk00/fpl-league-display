80fe07c1e00b6b5da578dbcef15c6c366f66a9ba
David Monk <david.gabriel.monk@cern.ch>
2024-09-05 10:36:04 +0200

    Added live updating to app

--- webapp/app.py
+++ webapp/app.py
@@ -2,66 +2,71 @@ from dash import Dash, html, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import os
 
-app = Dash(external_stylesheets=[dbc.themes.SANDSTONE], host='0.0.0.0')
+app = Dash(external_stylesheets=[dbc.themes.SANDSTONE])
 
-df = pd.read_csv("/files/stats.csv")
-df_table = df.copy()[df.event==df.event.max()][["player_name", "total_points"]]
-df_table = df_table.rename(columns={"player_name": "Player", "total_points": "Total Points"})
 
-app.layout = dbc.Container([
-    html.H1('FPL Mini League Stats'),
-    html.Hr(),
-    dbc.Table.from_dataframe(df_table, bordered=True),
-    
-    dbc.Row([
-        html.H2("Points")
-    ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(figure=px.bar(
-            df, x='event', y='points', color='player_name', barmode='group', 
-            labels={'event': 'Week', 'points': 'Gameweek Points', 'player_name': "Player"}
-        ))),
-        dbc.Col(dcc.Graph(figure=px.line(
-            df, x='event', y='total_points', color='player_name',
-            labels={'event': 'Week', 'total_points': 'Total Points', 'player_name': "Player"}
-        )))
-    ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(figure=px.line(
-            df, x='event', y='points_off_top', color='player_name',
-            labels={'event': 'Week', 'points_off_top': 'Points Off Top', 'player_name': "Player"}
-        )), width=6)
-    ]),
-    dbc.Row([
-        html.H2("Ranking")
-    ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(figure=px.bar(
-            df, x='event', y='rank', color='player_name', barmode='group', log_y=True,
-            labels={'event': 'Week', 'rank': 'Gameweek Rank', 'player_name': "Player"}
-        ))),
-        dbc.Col(dcc.Graph(figure=px.line(
-            df, x='event', y='overall_rank', color='player_name', log_y=True,
-            labels={'event': 'Week', 'overall_rank': 'Overall Rank', 'player_name': "Player"}
-        )))
-    ]),
-    dbc.Row([
-        html.H2("Finances")
-    ]),
-    dbc.Row([
-        dbc.Col([
-            dcc.Dropdown(["Total Value", "Team Value", "Bank"], "Total Value", id='finance-dropdown-selection'),
-            dcc.Graph(id="finance-graph")
-        ], width=6)
+def serve_layout():
+    df = pd.read_csv(f"{os.getenv('STATS_PATH')}/stats.csv")
+    df_table = df.copy()[df.event==df.event.max()][["player_name", "total_points"]]
+    df_table = df_table.rename(columns={"player_name": "Player", "total_points": "Total Points"})
+
+    layout = dbc.Container([
+        html.H1('FPL Mini League Stats'),
+        html.Hr(),
+        dbc.Table.from_dataframe(df_table, bordered=True),
+        
+        dbc.Row([
+            html.H2("Points")
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=px.bar(
+                df, x='event', y='points', color='player_name', barmode='group', 
+                labels={'event': 'Week', 'points': 'Gameweek Points', 'player_name': "Player"}
+            ))),
+            dbc.Col(dcc.Graph(figure=px.line(
+                df, x='event', y='total_points', color='player_name',
+                labels={'event': 'Week', 'total_points': 'Total Points', 'player_name': "Player"}
+            )))
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=px.line(
+                df, x='event', y='points_off_top', color='player_name',
+                labels={'event': 'Week', 'points_off_top': 'Points Off Top', 'player_name': "Player"}
+            )), width=6)
+        ]),
+        dbc.Row([
+            html.H2("Ranking")
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=px.bar(
+                df, x='event', y='rank', color='player_name', barmode='group', log_y=True,
+                labels={'event': 'Week', 'rank': 'Gameweek Rank', 'player_name': "Player"}
+            ))),
+            dbc.Col(dcc.Graph(figure=px.line(
+                df, x='event', y='overall_rank', color='player_name', log_y=True,
+                labels={'event': 'Week', 'overall_rank': 'Overall Rank', 'player_name': "Player"}
+            )))
+        ]),
+        dbc.Row([
+            html.H2("Finances")
+        ]),
+        dbc.Row([
+            dbc.Col([
+                dcc.Dropdown(["Total Value", "Team Value", "Bank"], "Total Value", id='finance-dropdown-selection'),
+                dcc.Graph(id="finance-graph")
+            ], width=6)
+        ])
     ])
-])
+    return layout
 
 @callback(
     Output('finance-graph', 'figure'),
     Input('finance-dropdown-selection', 'value')
 )
 def update_graph(value):
+    df = pd.read_csv(f"{os.getenv('STATS_PATH')}/stats.csv")
     match value:
         case "Total Value":
             dff = df[["value_mil", "player_name", "event"]]
@@ -87,6 +92,8 @@ def update_graph(value):
                 dff, x='event', y='value_mil', color='player_name',
                 labels={'event': 'Week', 'value_mil': 'Total Value (m)', 'player_name': "Player"}
             )
+            
+app.layout = serve_layout
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
